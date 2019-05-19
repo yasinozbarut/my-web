3fc09b96c6f0f986d8c7a22865f4dcc4f8fc7329
Yasin Emre OZBARUT <ysnemreozbarut@gmail.com>
2019-05-19 04:04:48 +0300

    Updates

---
+++ precache-manifest.62b80dabcb0fc1f430defbeeb6deb06a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0da7bcda2d778700f4733eb5fa7792b",
+    "url": "/my-web/index.html"
+  },
+  {
+    "revision": "23c055f927f2e8a07f56",
+    "url": "/my-web/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my-web/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "23c055f927f2e8a07f56",
+    "url": "/my-web/static/js/main.362b8d8f.chunk.js"
+  },
+  {
+    "revision": "f688b76d70f43d15fa5e",
+    "url": "/my-web/static/js/runtime~main.b2947276.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
